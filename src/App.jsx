1b61ee8b10c8affef827fee966854f6604877982
Purvi Porwal <purviporwal1812@gmail.com>
2024-07-08 17:28:15 +0530

    dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import { Outlet } from "react-router-dom";
+
+import React, { useState } from "react";
 
 function App() {
+  // const [theme, setTheme] = useState("light");
+
   return (
     <>
       <Navbar />
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,39 +1,62 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const CountryDetails = () => {
   const { countryName } = useParams();
-  const [countryData, setCountryData] = useState();
+  const [countryData, setCountryData] = useState(null);
+
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/name/${countryName}")
-      .then((res) => {
-        return res.json();
-      })
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+      .then((res) => res.json())
       .then((data) => {
-        return console.log(data);
         setCountryData(data[0]);
       });
   }, [countryName]);
+  const theme = "";
   return (
     <>
-      <div className="container">
-        country
-        <div className="card">
-          <img
-            src={countryData.flags.png}
-            className="card-img-top"
-            alt={countryData.flags.alt}
-          />
-          <div className="card-body">
-            <h5 className="card-title">{countryData.name.common}</h5>
-            <p className="card-text">Population: {countryData.population}</p>
-            <p className="card-text">Region: {countryData.region}</p>
-            <p className="card-text">Sub-Region: {countryData.subregion}</p>
-            <p className="card-text">Capital: {countryData.capital}</p>
+      <Navbar />
+      {countryData && (
+        <div className="container-fluid">
+          <div className="text-center mb-3">
+            <Link to="/" className="btn btn-primary">
+              Back to Countries
+            </Link>
+          </div>
+          <div className="container-fluid">
+            <div className="row justify-content-center align-items-center">
+              <div className="col-md-8">
+                <img
+                  src={countryData.flags.png}
+                  className="img-fluid rounded-start"
+                  alt={countryData.flags.alt}
+                />
+              </div>
+
+              <div className="col-md-4">
+                <h3>{countryData.name.common}</h3>
+                <p>
+                  <b>Population: </b>
+                  {countryData.population}
+                </p>
+                <p>
+                  <b>Region: </b>
+                  {countryData.region}
+                </p>
+                <p>
+                  <b>Sub-Region: </b>
+                  {countryData.subregion}
+                </p>
+                <p>
+                  <b>Capital: </b>
+                  {countryData.capital}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
